49ee903fffa4f878f0128cc988be207352d8cd14
Milan <milan.rockstar88@gmail.com>
2024-01-01 12:41:33 +0100

    Added console input so I can run any day

--- AdventOfCode2016/Program.cs
+++ AdventOfCode2016/Program.cs
@@ -2,33 +2,51 @@
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static void Run(int day)
         {
-            // * Day 1
-            // NoTimeForATaxicab noTimeForATaxicab = new NoTimeForATaxicab();
-            // noTimeForATaxicab.Part1();
-            // noTimeForATaxicab.Part2();
+            switch (day)
+            {
+                case 1:
+                    NoTimeForATaxicab noTimeForATaxicab = new NoTimeForATaxicab();
+                    noTimeForATaxicab.Part1();
+                    noTimeForATaxicab.Part2();
+                    break;
+                case 2:
+                    BathroomSecurity bathroomSecurity = new BathroomSecurity();
+                    bathroomSecurity.Part1();
+                    bathroomSecurity.Part2();
+                    break;
+                case 3:
+                    SquaresWithThreeSides squaresWithThreeSides = new SquaresWithThreeSides();
+                    squaresWithThreeSides.Part1();
+                    squaresWithThreeSides.Part2();
+                    break;
+                case 4:
+                    SecurityThroughObscurity securityThroughObscurity = new SecurityThroughObscurity();
+                    securityThroughObscurity.Part1();
+                    securityThroughObscurity.Part2();
+                    break;
+                case 5:
+                    HowAboutANiceGameOfChess howAboutANiceGameOfChess = new HowAboutANiceGameOfChess();
+                    howAboutANiceGameOfChess.Part1();
+                    howAboutANiceGameOfChess.Part2();
+                    break;
+            }
+        }
 
-            // * Day 2
-            // BathroomSecurity bathroomSecurity = new BathroomSecurity();
-            // bathroomSecurity.Part1();
-            // bathroomSecurity.Part2();
+        public static void Main()
+        {
+            Console.WriteLine("Please enter a number from 1 to 25:");
 
-            // * Day 3
-            // SquaresWithThreeSides squaresWithThreeSides = new SquaresWithThreeSides();
-            // squaresWithThreeSides.Part1();
-            // squaresWithThreeSides.Part2();
+            string input = Console.ReadLine() ?? "";
 
-            // * Day 4
-            // SecurityThroughObscurity securityThroughObscurity = new SecurityThroughObscurity();
-            // securityThroughObscurity.Part1();
-            // securityThroughObscurity.Part2();
+            if (!int.TryParse(input, out int day) || day < 1 || day > 25)
+            {
+                Console.WriteLine("Invalid input. Please provide a valid number from 1 to 25.");
+                return;
+            }
 
-            // * Day 5
-            // HowAboutANiceGameOfChess howAboutANiceGameOfChess = new HowAboutANiceGameOfChess();
-            // howAboutANiceGameOfChess.Part1();
-            // howAboutANiceGameOfChess.Part2();
-
+            Run(day);
         }
     }
 }
